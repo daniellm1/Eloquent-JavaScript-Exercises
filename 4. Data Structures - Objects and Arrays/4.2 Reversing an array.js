14b8fd7919bca881717589da5d4ff8fd7a597346
Daniel López <danielm109000@gmail.com>
2022-03-22 20:14:38 +0100

    solved

---
+++ 4. Data Structures - Objects and Arrays/4.2 Reversing an array.js
@@ -0,0 +1,24 @@
+const reverseArray = (arr) => {
+    let result = [];
+    for (let i in arr) {
+        result.unshift(arr[i]);
+    }
+    return result;
+}
+
+const reverseArrayInPlace = (arr) => {
+    const midArrLength = Math.floor(arr.length / 2);
+    const lastIndex = arr.length - 1;
+    for (let i = 0; i < midArrLength; i++) {
+        let arrBinding = arr[i];
+        arr[i] = arr[lastIndex - i];
+        arr[lastIndex - i] = arrBinding;
+    }
+}
+
+console.log(reverseArray(["A", "B", "C"]));
+// → ["C", "B", "A"];
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+// → [5, 4, 3, 2, 1]
